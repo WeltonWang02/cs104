3b250cc644b61232d0abbc01ad39c23b9d90117e
Welton Wang <weltonwang88@gmail.com>
2026-05-09 12:13:10 -0700

    cpp

---
+++ mathfunc.cpp
@@ -0,0 +1,18 @@
+#include "mathfuncs.h"
+#include <stdexcept>
+
+double subtract(double a, double b) {
+    return a - b;
+}
+
+double multiply(double a, double b) {
+    return a * b;
+}
+
+double divide(double a, double b) {
+    if (b == 0) {
+        throw std::invalid_argument("cannot divide by zero");
+    }
+
+    return a / b;
+}
