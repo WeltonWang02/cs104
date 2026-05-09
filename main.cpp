e543995260d771279b66d62937b7c5b17bd0b66c
Gabriel Bo <gabe0496soccer@gmail.com>
2026-05-09 15:28:30 -0700

    wire flip, d6, and d20 commands into calculator

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 #include <string>
 #include "mathfuncs.h"
+#include "randfuncs.h"
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
+    srand(static_cast<unsigned>(time(nullptr)));
+
     const string EXIT = "quit";
     string command;
 
@@ -20,6 +24,9 @@ int main(int argc, char *argv[]) {
             cout << "  subtract <a> <b>     Subtract b from a" << endl;
             cout << "  multiply <a> <b>     Multiply a and b" << endl;
             cout << "  divide <a> <b>       Divide a by b" << endl;
+            cout << "  flip                 Flip a coin (0=tails, 1=heads)" << endl;
+            cout << "  d6                   Roll a six-sided die" << endl;
+            cout << "  d20                  Roll a twenty-sided die" << endl;
             cout << endl;
         }
         else if ("subtract" == command) {
@@ -42,6 +49,15 @@ int main(int argc, char *argv[]) {
             catch (const invalid_argument& e) {
                 cout << "error: " << e.what() << endl;
             }
+        }
+        else if ("flip" == command) {
+            cout << flip_coin() << endl;
+        }
+        else if ("d6" == command) {
+            cout << roll_d6() << endl;
+        }
+        else if ("d20" == command) {
+            cout << roll_d20() << endl;
         }
         else if (EXIT == command) {
         }
